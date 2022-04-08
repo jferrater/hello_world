e7d21e63a676dd1c5a3ae84bb5ae81f2fe8e9f29
Joffry Ferrater <joffry.ferrater@gmail.com>
2022-04-09 00:08:59 +0200

    Enable prometheus client and listening on port 9095 by default

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
+import os
 import requests
 from flask import Flask, jsonify, request
+from prometheus_client import start_wsgi_server
+
 
 app = Flask(__name__)
+start_wsgi_server(int(os.environ.get('PROMETHEUS_PORT', 9095)))
 
 
 @app.route('/', methods=['POST'])
